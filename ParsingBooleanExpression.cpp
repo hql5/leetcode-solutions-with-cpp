c546f6a4d887821d212466d4a627f9033b4d9139
lhq-lhq <1901212116@qq.com>
2022-11-05 17:52:10 +0800

    #1106

---
+++ ParsingBooleanExpression.cpp
@@ -0,0 +1,51 @@
+/*
+ * @Author: lhq
+ * @Date: 2022-11-05 17:48:48
+ * @LastEditTime: 2022-11-05 17:51:45
+ * @LastEditors: lhq
+ * @Description: 解析布尔表达式，利用栈
+ * @FilePath: \leetcode-solutions-with-cpp\ParsingBooleanExpression.cpp
+ */
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        stack<char> stk;
+        int n = expression.size();
+        for (int i = 0; i < n; i++) {
+            char c = expression[i];
+            if (c == ',') {
+                continue;
+            } else if (c != ')') {
+                stk.push(c);
+            } else {
+                int t = 0, f = 0;
+                while (stk.top() != '(') {
+                    char val = stk.top();
+                    stk.pop();
+                    if (val == 't') {
+                        t++;
+                    } else {
+                        f++;
+                    }
+                }
+                stk.pop();
+                char op = stk.top();
+                stk.pop();
+                switch (op) {
+                case '!':
+                    stk.push(f == 1 ? 't' : 'f');
+                    break;
+                case '&':
+                    stk.push(f == 0 ? 't' : 'f');
+                    break;
+                case '|':
+                    stk.push(t > 0 ? 't' : 'f');
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        return stk.top() == 't';
+    }
+};
